14016b709456ecbaae054546b83c070aab868151
Carlos Prieto <cprieto@apexsystems.com>
2026-09-24 16:25:06 -0600

    Create React frontend skeleton

---
+++ src/frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <main>
+      <h1>ServiceDesk</h1>
+      <p>IT Support Management System</p>
+      <p>Frontend application is running.</p>
+    </main>
+  )
+}
+
+export default HomePage
